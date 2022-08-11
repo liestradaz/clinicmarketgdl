0b59fa2ae2f56695de049e7ab3d73ea57dfd73e1
nash0x <nachoest@gmail.com>
2022-08-11 15:43:56 -0600

    changed data in sticky bar

--- src/components/StickyBar.jsx
+++ src/components/StickyBar.jsx
@@ -8,7 +8,6 @@ import {
   Stack,
   useBreakpointValue,
 } from "@chakra-ui/react";
-import { BsWhatsapp } from "react-icons/bs";
 
 export default function StickyBar() {
   return (
@@ -23,38 +22,10 @@ export default function StickyBar() {
         style={{ top: "0.1px", zIndex: "3" }}
         /* bgGradient="linear(to-l, blue.300, cyan.500)" */
       >
-        <Stack
-          align={"center"}
-          spacing={2}
-          justify={"center"}
-          direction={useBreakpointValue(
-            {
-              base: "column",
-              sm: "column",
-              md: "row",
-              lg: "row",
-              xl: "row",
-              "2xl": "row",
-            },
-            { ssr: false }
-          )}
-        >
-          <Text fontWeight={"500"}>
-            ¡Servicio a domicilio sin costo en el Área Metropolitana de
-            Guadalajara!
-          </Text>
-          <HStack alignItems="Stack-start">
-            <Icon as={BsWhatsapp} mt={1} />
-            <Link
-              href="https://api.whatsapp.com/send?phone=523322579557"
-              isExternal
-            >
-              <Text as={"i"} fontWeight={"500"}>
-                33 2257 9557
-              </Text>
-            </Link>
-          </HStack>
-        </Stack>
+        <Text fontWeight={"500"}>
+          ¡Servicio a domicilio sin costo en el Área Metropolitana de
+          Guadalajara!
+        </Text>
       </Box>
     </>
   );
